1b912d0225024c0d19c7e926c492e78ef80f5317
matsumoto-kenta <k.omustam@gmail.com>
2021-10-20 22:18:13 +0900

    application.ymlまで修正

---
+++ src/main/java/jp/co/planaria/favoriteanime/FavoriteAnimeApplication.java
@@ -0,0 +1,13 @@
+package jp.co.planaria.favoriteanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/planaria/favoriteanime/FavoriteAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.planaria.favoriteanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
